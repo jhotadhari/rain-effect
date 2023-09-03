3bc5ac1bc9e7d3e059ae55fad94c0e44385e42d1
jhotadhari <tellme@waterproof-webdesign.info>
2023-09-03 02:47:51 +0200

    Updated to generator-wp-dev-env#1.6.8 ( wp-dev-env-grunt#1.6.2 wp-dev-env-frame#0.16.0 )

--- src/classes/Rain.php
+++ src/classes/Rain.php
@@ -11,8 +11,25 @@ use croox\wde;
 
 class Rain extends wde\Plugin {
 
+	public function initialize() {
+
+		// // Run Updates when plugin version changes.
+		// add_filter( $this->prefix . '_update_version', function( $success, $new_version, $old_version ) {
+		// 	return $success;
+		// }, 10, 3 );
+
+		// // Run Updates when plugin db_version changes.
+		// add_filter( $this->prefix . '_update_db_version', function( $success, $new_db_version, $old_db_version ) {
+		// 	return $success;
+		// }, 10, 3 );
+
+		parent::initialize();
+	}
+
 	public function hooks(){
         parent::hooks();
+
+		// $this->_include( 'css_properties' ); // Includes function rain_get_css_property
 
         // // Fix WPML global active language variable for REST Requests.
         // if ( class_exists( 'SitePress' ) ) {
